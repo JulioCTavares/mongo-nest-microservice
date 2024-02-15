828fa42d9bc335c246e26798248e678834e0740e
Julio Tavares <juliotavares197@gmail.com>
2024-02-15 18:07:12 -0300

    feat: create zod validation pipe

--- apps/orders/src/env.ts
+++ apps/orders/src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const EnvSchema = z.object({
-  MONGO_URI: z.string().url(),
+  MONGODB_URI: z.string().url(),
 });
 
 export type Env = z.infer<typeof EnvSchema>;
